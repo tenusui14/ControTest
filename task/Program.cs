0d3ef587c686d415cbd513cbacd78788f4203f59
ArtemTsytsylin <workhardgofock@gmail.com>
2023-07-29 21:39:58 +0300

    task is done

---
+++ task/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] someArgs = { "12345", "ads", "Portal", "+++", "md", "3", "a", "fffff", "00", "fdsgads", "kek" };
+
+Console.WriteLine($"[{string.Join(", ", someArgs)}]");
+string[] outputArray = Array.Empty<string>();
+int j = 0;
+Array.Resize(ref outputArray, outputArray.Length + 1);
+for (int i = 0; i < someArgs.Length; i++)
+{
+
+    if (someArgs[i].Length <= 3)
+    {
+        outputArray[j] = someArgs[i];
+        j++;
+        Array.Resize(ref outputArray, outputArray.Length + 1);
+    }
+}
+Array.Resize(ref outputArray, outputArray.Length - 1);
+Console.WriteLine($"[{string.Join(", ", outputArray)}]");
